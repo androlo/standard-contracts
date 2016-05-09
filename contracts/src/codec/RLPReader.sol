c9975d38a5e1f2a8d58144cf81ffb8d3d7dac835
androlo <androlo1980@gmail.com>
2016-05-09 13:47:14 +0200

    .

--- contracts/src/codec/RLPReader.sol
+++ contracts/src/codec/RLPReader.sol
@@ -22,7 +22,11 @@
  *
  * When it comes to the sub-items, toRLPItem(item, 0) would be the RLP encoding
  * of "haha", and toRLPItem(item, 1) would be the RLP encoding of ["lala"]. Finally,
- * toRLPItem(toRLPItem(item, 1), 0) would be the RLP encoding of "lala".
+ * toRLPItem(toRLPItem(item, 1), 0) would be the RLP encoding of "lala". Extracting
+ * items is done by slicing, so does not create any new byte arrays.
+ *
+ * RLP-items that are strings can be decoded into a 'bytes memory'. This will copy
+ * the bytes.
  */
 library RLPReader {
 
@@ -38,30 +42,80 @@ library RLPReader {
     }
 
     /// @dev Creates an RLPItem from an array of RLP encoded bytes.
-    /// @dev The RLP encoded bytes.
+    /// @param self The RLP encoded bytes.
     /// @return An RLPItem
-    function toRLPItem(bytes memory rlp) internal constant returns (RLPItem memory item) {
+    function toRLPItem(bytes memory self) internal constant returns (RLPItem memory) {
         uint start;
         assembly {
-            start := add(rlp, 0x20)
+            start := add(self, 0x20)
         }
-        item = _toRLPItem(start);
+        return _toRLPItem(start);
+    }
+
+    /// @dev Check if the RLP item is a list.
+    /// @param self The RLP item.
+    /// @return 'true' if it is a list, 'false' if it is a string.
+    function isList (RLPItem memory self) internal constant returns (bool) {
+        return self._unsafe_isList;
+    }
+
+    /// @dev Get the number of items in an RLP encoded list.
+    /// @param self The RLP item.
+    /// @return The number of items. Strings will return 0; checking if
+    /// the item is a list is done by calling 'RLPItem.isList'.
+    function numItems (RLPItem memory self) internal constant returns (uint) {
+        return self._unsafe_listLength;
     }
 
     /// @dev Creates an RLPItem from a sub-item inside an existing RLPItem.
     /// The current RLPItem must be of list type.
-    /// @param rlpItem The RLPItem
+    /// @param self The RLPItem
     /// @param index The index of the sub-item in the list.
     /// @return A new RLPItem.
-    function toRLPItem(RLPItem memory rlpItem, uint index) internal constant returns (RLPItem memory item) {
-        if(index >= rlpItem.len)
+    function item(RLPItem memory self, uint index) internal constant returns (RLPItem memory) {
+        if(index >= self._unsafe_length)
             throw;
         uint bPos;
-        uint listStart = rlpItem.listStart;
+        uint listStart = self._unsafe_listPtr;
         assembly {
             bPos := mload(add(listStart, mul(index, 0x20)))
         }
-        item = _toRLPItem(bPos);
+        return _toRLPItem(bPos);
+    }
+
+    /// @dev Decode an RLPItem into bytes. This will not work if the
+    /// RLPItem is a list.
+    /// @param self The RLPItem.
+    /// @return The decoded string.
+    function decode(RLPItem memory self) internal constant returns (bytes memory bts) {
+        if(self._unsafe_isList)
+            throw;
+        uint len;
+        uint b0;
+        uint start = self._unsafe_memPtr;
+        assembly {
+            b0 := byte(0, mload(start))
+        }
+        if (b0 < 0x80) {
+            bts = new bytes(1);
+            bts[0] = byte(b0);
+            return;
+        }
+        uint rStartPos; // Where to start reading.
+
+        if (b0 < 0xB8) {
+            len = self._unsafe_length - 1;
+            rStartPos = start + 1;
+        } else {
+            uint bLen;
+            assembly {
+                bLen := sub(b0, 0xB7)
+            }
+            len = self._unsafe_length - 1 - bLen;
+            rStartPos = start + bLen + 1;
+        }
+        bts = new bytes(len);
+        _copyToBytes(rStartPos, bts, len);
     }
 
     /// @dev Creates an RLPItem from a memory address.
@@ -78,7 +132,7 @@ library RLPReader {
             return;
         }
         if (b0 < 0xB8) {
-            item = RLPItem(start, 1 + (b0 - 0x80), false, 0, 0);
+            item = RLPItem(start, b0 - 0x80, false, 0, 0);
             return;
         }
         if (b0 < 0xC0) {
@@ -126,92 +180,17 @@ library RLPReader {
         item = RLPItem(start, len, true, listStart, listLen);
     }
 
-    /// @dev encode a string to an RLP item. This does not work for lists
-    /// since there is no way to process arbitrary RLP encode-able data.
-    /// @param data An RLP encoded string.
-    /// @return The RLPItem.
-    function encode(bytes memory data) internal constant returns (RLPItem) {
-        bytes memory encoded;
-        var dataL = data.length;
-        if (dataL == 1 && data[0] < 128)
-            encoded = data;
-        else {
-            uint iOffset;
-            if (dataL < LENGTH_THRESHOLD) {
-                iOffset = 1;
-                encoded = new bytes(dataL + 1);
-                encoded[0] = byte(data.length + 128);
-            } else if (dataL < MAX_LENGTH) {
-                uint bLen;
-                assembly {
-                    bLen := add(div(dataL, 256), gt(mod(dataL, 256), 0))
-                }
-                iOffset = bLen + 1;
-                encoded = new bytes(dataL + 1 + bLen);
-                encoded[0] = byte(bLen + 184);
-                // Write the length in encoding[1], ... , encoding[bLen]
-                assembly {
-                    mstore(add(encoded, 33), mul(dataL, exp(256, sub(32, bLen))))
-                }
-            } else
-                throw;
-            uint dataPtr;
-            uint copyLen;
-            assembly {
-                dataPtr := add(data, 0x20)
-                copyLen := mload(data)
-            }
-            _copyToBytes(dataPtr, encoded, iOffset, copyLen);
-
-        }
-        return RLPItem(dataPtr, copyLen, false, 0, 0);
-    }
-
-    /// @dev Decode an RLPItem into bytes. This will not work if the
-    /// RLPItem is a list.
-    /// @param rlpItem The RLPItem.
-    /// @return The decoded string.
-    function decode(RLPItem memory rlpItem) internal constant returns (bytes memory bts) {
-        if(rlpItem.isList)
-            throw;
-        uint len;
-        uint b0;
-        uint start = rlpItem._unsafe_memPtr;
-        assembly {
-            b0 := byte(0, mload(start))
-        }
-        if (b0 < 0x80) {
-            bts = new bytes(1);
-            bts[0] = byte(b0);
-            return;
-        }
-        uint rStartPos; // Where to start reading.
-
-        if (b0 < 0xB8) {
-            len = b0 - 0x80;
-            rStartPos = start + 1;
-        } else {
-            assembly {
-                let bLen := sub(b0, 0xB7) // bytes length
-                let len := div(mload(add(start, 1)), exp(256, sub(32, bLen))) // data length
-                rStartPos := add(add(start, 1), bLen)
-            }
-        }
-
-        bts = new bytes(len);
-        _copyToBytes(rStartPos, bts, 0, len);
-    }
 
     // Assumes that enough memory has been allocated to store in target.
-    function _copyToBytes(uint btsPtr, bytes memory tgt, uint tgtStartPos, uint btsLen) constant private {
+    function _copyToBytes(uint btsPtr, bytes memory tgt, uint btsLen) internal constant {
         // Exploiting the fact that 'tgt' was the last thing to be allocated,
         // we can write entire words, and just overwrite any excess.
         assembly {
             {
                     let i := 0 // Start at arr + 0x20
-                    let words := add(div(btsLen, 32), gt(mod(btsLen, 32), 0))
+                    let words := div(add(btsLen, 31), 32)
                     let rOffset := btsPtr
-                    let wOffset := add(tgt, add(tgtStartPos, 0x20))
+                    let wOffset := add(tgt, 0x20)
                 tag_loop:
                     jumpi(end, eq(i, words))
                     {
